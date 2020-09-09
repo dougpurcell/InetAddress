302368e2f711acd2ecab656eb1ea0f29a9b92d85
Doug Griffiths <doug@Dougs-MacBook-Pro.local>
2020-09-09 17:02:35 -0400

    First commit - mostly working

---
+++ src/App.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class App {
+    private JPanel mainPanel;
+    private JButton dQuadToSymButton;
+    private JButton symToDQuadButton;
+    private JLabel dQuadLabel;
+    private JLabel symbolicLabel;
+    private JTextField symbolicText;
+    private JTextField dQuadText;
+
+    public App() {
+        // TODO: controlling input + button enabling for dQuad.
+        // TODO: controlling input + button enabling for symbolic.
+        // TODO: limit dQuad input to just IPs
+        // TODO: OPTIONAL limit symbolic input to legit web addresses?
+        // TODO: OPTIONAL display errors and BS to user instead of in console.
+
+        dQuadToSymButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    symbolicText.setText(dQuadInet(dQuadText.getText()));
+                    System.out.println("symbolic is " + dQuadInet(dQuadText.getText()));
+
+                } catch (UnknownHostException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        symToDQuadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    dQuadText.setText(symbolicInet(symbolicText.getText()));
+                    System.out.println("dQuad is " + symbolicInet(symbolicText.getText()));
+
+                } catch (UnknownHostException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    // dQuad Input (127.01.01.01), Symbolic output
+    public String dQuadInet(String s) throws UnknownHostException {
+        try {
+            InetAddress dQuadInet = InetAddress.getByName(s);
+            String s1 = dQuadInet.getHostName();
+            return s1;
+        }
+        catch (UnknownHostException ex) {
+            return "Couldn't find that Host Name.";
+        }
+    }
+
+    // Symbolic Input (www.example.com), DQuad output
+    public String symbolicInet(String s) throws UnknownHostException {
+        try {
+            InetAddress symbolicInet = InetAddress.getByName(s);
+            String s1 = symbolicInet.toString();
+            String s2 = s1.substring(s1.indexOf("/") + 1);
+            return s2;
+        }
+        catch (UnknownHostException ex) {
+            return "Couldn't find that URL.";
+        }
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("InetAddress");
+        frame.setContentPane(new App().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+        frame.setVisible(true);
+    }
+}
